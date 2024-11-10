bd5ae70a614d8b85f72706e8307888d333732f87
yash-learner <yash.topaz@gmail.com>
2024-11-10 20:49:33 +0530

    Replace controls.jsx with controls.tsx, adding greeting prop and TypeScript support

--- src/components/controls.tsx
+++ src/components/controls.tsx
@@ -1,13 +1,12 @@
-import PropTypes from 'prop-types';
 
-const ControlPanel = ({ name, onChange }) => {
+const ControlPanel = ({ name, greeting, onChange}: {name: string, greeting: string, onChange: React.ChangeEventHandler<HTMLInputElement>}) => {
   return (
     <form
       className="flex flex-row gap-4 text-white bg-primary-10"
       onSubmit={(event) => event.preventDefault()}
     >
       <div>
-        <label className="font-bold">Your Name</label>
+        <label className="font-bold">{greeting} {name}</label>
         <input
           name="name"
           className="w-full"
@@ -18,11 +17,6 @@ const ControlPanel = ({ name, onChange }) => {
       </div>
     </form>
   );
-};
-
-ControlPanel.propTypes = {
-  name: PropTypes.string,
-  onChange: PropTypes.func,
 };
 
 export default ControlPanel;
